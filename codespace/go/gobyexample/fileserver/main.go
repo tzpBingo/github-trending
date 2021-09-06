08a3eaaf2e4a590364b44cc66f2ada18758425d1
tzpbingo <tzpbingo@gmail.com>
2021-09-06 17:46:33 +0800

    Edit golang

--- codespace/go/gobyexample/fileserver/main.go
+++ codespace/go/gobyexample/fileserver/main.go
@@ -10,6 +10,7 @@ func main() {
 	http.Handle("/ucd/", http.StripPrefix("/ucd/", http.FileServer(http.Dir("./"))))
 
 	err := http.ListenAndServe(":11080", nil)
+	
 	if err != nil {
 		fmt.Println(err)
 	}
