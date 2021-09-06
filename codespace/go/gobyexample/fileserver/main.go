341d970c2f609f4c85fb337a3ed378c76299bb1b
tzpbingo <tzpbingo@gmail.com>
2021-09-06 17:39:00 +0800

    Add golang type

---
+++ codespace/go/gobyexample/fileserver/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.Handle("/ucd/", http.StripPrefix("/ucd/", http.FileServer(http.Dir("./"))))
+
+	err := http.ListenAndServe(":11080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
